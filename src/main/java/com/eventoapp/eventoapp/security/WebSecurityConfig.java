adc08aa49aad57fb49c5c4d1d35ad7a0b27942d1
alexisbrabo <alexis_brabo@hotmail.com>
2018-04-26 10:34:01 -0300

    Correção na Segurança

--- src/main/java/com/eventoapp/eventoapp/security/WebSecurityConfig.java
+++ src/main/java/com/eventoapp/eventoapp/security/WebSecurityConfig.java
@@ -21,7 +21,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/").permitAll()
                 .antMatchers(HttpMethod.GET, "/cadastrarEvento").hasRole("ADMIN")
                 .antMatchers(HttpMethod.POST, "/cadastrarEvento").hasRole("ADMIN")
                 .anyRequest().authenticated()
